ae9308e7eb57fcf8fc3d0959fa654a0bd45447ac
emreakay <emreakay@users.noreply.github.com>
2023-07-31 20:19:21 +0000

    Fix styling

--- config/acalendar.php
+++ config/acalendar.php
@@ -7,7 +7,7 @@ return [
         'all_day_event_start_time' => '09:00',
         'all_day_event_end_time' => '22:00',
         'date_event_duration_min' => 30,
-        'datetime_event_duration_min' => 30
-    ]
+        'datetime_event_duration_min' => 30,
+    ],
 
 ];
