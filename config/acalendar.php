4354a923a98cb1f574a9bc11d53a1400c2e0f5da
emreakay <emre.akay@aurorabilisim.com>
2023-07-31 23:18:55 +0300

    bismillah

--- config/acalendar.php
+++ config/acalendar.php
@@ -3,4 +3,11 @@
 // config for AuroraWebSoftware/ACalendar
 return [
 
+    'calendar' => [
+        'all_day_event_start_time' => '09:00',
+        'all_day_event_end_time' => '22:00',
+        'date_event_duration_min' => 30,
+        'datetime_event_duration_min' => 30
+    ]
+
 ];
